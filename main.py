d23c1df9be1efe2c606b56d9d3a899cefeb155b8
Gökmen Görgen <gkmngrgn@gmail.com>
2011-07-24 14:38:38 +0300

    templateview.

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import os
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import util
 
+class TemplateView(webapp.RequestHandler):
 
-class MainHandler(webapp.RequestHandler):
+    template_name = None
+    template_path = os.path.join(os.path.dirname(__file__), 'views')
+
+    def get_template(self):
+        try:
+            return os.path.join(self.template_path, '%s.html' % self.template_name)
+        except AttributeError:
+            return None
+
+    def get_template_values(self):
+        return {}
+
     def get(self):
-        self.response.out.write('Hello world!')
+        context = template.render(self.get_template(), self.get_template_values())
+
+        self.response.out.write(context)
+
+
+class MainHandler(TemplateView):
+
+    template_name = 'home/index'
+
+    def get_template_values(self):
+        template_values = {
+            'welcome': 'Welcome, user.'
+        }
+
+        return template_values
 
 
 def main():
